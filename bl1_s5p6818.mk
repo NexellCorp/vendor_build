ede8577bbf0fae9ca316f2a18a03daa191e7581d
Youngdo, Lee <ydlee@nexell.co.kr>
2020-03-23 10:07:24 +0900

    build: Add PRODCUT_CAR option
    Export PRODUCT_CAR option to determine this build is targetting to the
    automotive image or not.
    
    Signed-off-by: Youngdo, Lee <ydlee@nexell.co.kr>
    Change-Id: I46cb406964aad5155b6c754ae184f5fee6cd01ab

--- bl1_s5p6818.mk
+++ bl1_s5p6818.mk
@@ -5,7 +5,7 @@ BL1_BIN := $(PRODUCT_OUT)/bl1_emmcboot.bin
 $(BL1_BIN) :
 	$(hide) echo "=========== BL1 SDMMC boot Building !!! ==========";
 	$(MAKE) -C $(TARGET_BL1_SRC) clean;
-	$(MAKE) -C $(TARGET_BL1_SRC) BOARD="$(BL1_BOARD_NAME)" KERNEL_VER="4" SYSLOG="n" DEVICE_PORT="$(DEVICE_PORT)" SECURE_ON=1 QUICKBOOT="$(QUICKBOOT)" SUPPORT_OTA_AB_UPDATE=y;
+	$(MAKE) -C $(TARGET_BL1_SRC) BOARD="$(BL1_BOARD_NAME)" KERNEL_VER="4" SYSLOG="n" DEVICE_PORT="$(DEVICE_PORT)" SECURE_ON=1 QUICKBOOT="$(QUICKBOOT)" SUPPORT_OTA_AB_UPDATE=y PRODUCT_CAR="$(PRODUCT_CAR)";
 	$(shell cp -af $(TARGET_BL1_SRC)/out/bl1-$(BL1_BOARD_NAME).bin $(PRODUCT_OUT)/bl1-usbboot.bin);
 	$(shell cp -af $(TARGET_BL1_SRC)/out/bl1-emmcboot.bin $(PRODUCT_OUT)/bl1-emmcboot.bin);
 
